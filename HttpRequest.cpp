6a3790586be62167040df09b4e57dc991063448c
haochiencho@gmail.com <haochiencho@gmail.com>
2017-01-22 13:46:23 -0800

    HttpRequest base class

---
+++ HttpRequest.cpp
@@ -0,0 +1,12 @@
+//
+//  codeForce practice
+//
+//  Created by Brian Cho on 1/22/17.
+//  Copyright © 2017 Cho. All rights reserved.
+//
+
+#include "HttpRequest.h"
+
+HttpRequest::HttpRequest(std::string request){
+  
+}
---
+++ HttpRequest.h
@@ -0,0 +1,33 @@
+//
+//  codeForce practice
+//
+//  Created by Brian Cho on 1/22/17.
+//  Copyright © 2017 Cho. All rights reserved.
+//
+
+#ifndef HttpRequest_h
+#define HttpRequest_h
+#include <stdio.h>
+#include <string>
+#include <iostream>
+#include <vector>
+
+class HttpRequest{
+public:
+  HttpRequest(std::string request);
+  std::string getRequest();
+  std::vector<std::string> getHeaders();
+  std::string getBody();
+  std::string getProtocol();
+  std::string getPort();
+  std::string getPath();
+private:
+  std::string m_Request;
+  std::vector<std::string> m_header;
+  std::string m_body;
+  std::string m_protocol;
+  std::string m_port;
+  std::string m_path;
+};
+
+#endif /* HttpRequest_h */
