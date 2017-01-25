62978bf150706ef73d8d9f3a67d9244d8b86deda
Usama Riaz <u3amar@hotmail.com>
2017-01-24 23:51:22 -0800

    Added in basic error checking

--- ConnectionManager.cpp
+++ ConnectionManager.cpp
@@ -3,12 +3,16 @@
 // Copyright © 2017 Riaz. All rights reserved.
 //
 
+#include <iostream>
 #include "ConnectionManager.h"
-#include "HttpResponse.h"
 
 const std::string PROTOCOL_VERSION = "Http/1.1";
+
 const std::string SUCCESS_MESSAGE = "OK";
 const unsigned SUCCESS = 200;
+
+const std::string BAD_REQUEST_MESSAGE = "Bad Request";
+const unsigned BAD_REQUEST = 400;
 
 ConnectionManager::ConnectionManager(unsigned port_number) {
   port_number_ = port_number;
@@ -28,15 +32,21 @@ void ConnectionManager::RunTcpServer() {
     boost::asio::streambuf buffer;
     boost::system::error_code error;
     size_t len = read_until(socket, buffer, REQUEST_DELIMITER, error);
-       
+    
+    StatusLine *resp_status;
+    std::string resp_body = "";
+
     if (len) {
       message_stream.write(boost::asio::buffer_cast<const char *>(buffer.data()), len);
-      StatusLine resp_status(PROTOCOL_VERSION, SUCCESS, SUCCESS_MESSAGE);
-      HttpResponse response(resp_status);
-      response.AddHeader("Content-Type: text/plain");
-      response.SetBody(message_stream.str());
-      
-      boost::asio::write(socket, boost::asio::buffer(response.Serialize()));
+      resp_body = message_stream.str();
+      resp_status = new StatusLine(PROTOCOL_VERSION, SUCCESS, SUCCESS_MESSAGE);
+    } else {
+      resp_status = new StatusLine(PROTOCOL_VERSION, BAD_REQUEST, BAD_REQUEST_MESSAGE);
     }
+
+    HttpResponse response(*resp_status);
+    response.AddHeader("Content-Type: text/plain");
+    response.SetBody(resp_body);
+    boost::asio::write(socket, boost::asio::buffer(response.Serialize()));
   }
 }
--- ConnectionManager.h
+++ ConnectionManager.h
@@ -6,15 +6,17 @@
 #ifndef ConnectionManager_h
 #define ConnectionManager_h
 
-#include <iostream>
 #include <string>
 #include <boost/asio.hpp>
+#include "HttpResponse.h"
 
 class ConnectionManager {
  public:
   ConnectionManager(unsigned port_number);
   void RunTcpServer();
  private:
+  void CreateBasicResponse(HttpResponse &res);
+  void CreateBadResponse(HttpResponse &res);
   unsigned port_number_;
 };
 
