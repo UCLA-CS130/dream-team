5fc261d519da01bc1434b9e1c027f27b84b9b660
Usama Riaz <u3amar@hotmail.com>
2017-01-24 18:11:26 -0800

    Built basic implementation of HttpMessage

--- HttpMessage.cpp
+++ HttpMessage.cpp
@@ -3,37 +3,20 @@
 // Copyright © 2017 Riaz. All rights reserved.
 //
 
-#ifndef HttpMessage_h
-#define HttpMessage_h
+#include "HttpMessage.h"
 
-#include <string>
-#include <vector>
+HttpMessage::HttpMessage(std::string start_line) {
+  start_line_ = start_line;
+}
 
-/*
-From Apache HttpComponents:
+void HttpMessage::addHeader(std::string header) {
+  headers_.push_back(header);
+}
 
-HTTP messages use the generic message format of RFC 822 for
-transferring entities (the payload of the message). Both 
-types of message consist of a start-line, zero or more header
-fields (also known as "headers"), an empty line (i.e., a line 
-with nothing preceding the CRLF) indicating the end of the 
-header fields, and possibly a message-body.
+std::vector<std::string> HttpMessage::getHeaders() {
+  return headers_;
+}
 
-      generic-message = start-line
-                        *(message-header CRLF)
-                        CRLF
-                        [ message-body ]
-      start-line      = Request-Line | Status-Line
-*/
-class HttpMessage {
-public:
-  HttpMessage(std::string start_line);
-  void addHeader(std::string header);
-  std::vector<std::string> getHeaders();
-  std::string getBody();
-private:
-  std::string start_line_;
-  std::vector<std::string> headers_;
-};
-
-#endif
+std::string HttpMessage::getBody() {
+  return body_;
+}
---
+++ HttpMessage.h
@@ -0,0 +1,40 @@
+//
+// Created by Usama Riaz on 1/24/17.
+// Copyright © 2017 Riaz. All rights reserved.
+//
+
+#ifndef HttpMessage_h
+#define HttpMessage_h
+
+#include <string>
+#include <vector>
+
+/*
+From Apache HttpComponents:
+
+HTTP messages use the generic message format of RFC 822 for
+transferring entities (the payload of the message). Both 
+types of message consist of a start-line, zero or more header
+fields (also known as "headers"), an empty line (i.e., a line 
+with nothing preceding the CRLF) indicating the end of the 
+header fields, and possibly a message-body.
+
+      generic-message = start-line
+                        *(message-header CRLF)
+                        CRLF
+                        [ message-body ]
+      start-line      = Request-Line | Status-Line
+*/
+class HttpMessage {
+public:
+  HttpMessage(std::string start_line);
+  void addHeader(std::string header);
+  std::vector<std::string> getHeaders();
+  std::string getBody();
+private:
+  std::string start_line_;
+  std::string body_;
+  std::vector<std::string> headers_;
+};
+
+#endif
