41067fa1d9bb04a2e89c2b3b0999703e2f79605c
Aditya Srinivasan <asrinivasan@ucla.edu>
2017-01-31 00:28:51 -0800

    Basic integration test code added, need to flesh it out more

--- integration_tests.py
+++ integration_tests.py
@@ -1,3 +1,14 @@
 #!/usr/bin/env python
 
-# Add integration tests
+import os.path, subprocess, urllib
+
+if not os.path.isfile("config_file"):
+    print "The config file does not exist - aborting tests"
+    sys.exit(1)
+
+running_server = subprocess.Popen(['./webserver', 'config_file'])
+response = urllib.urlopen('http://localhost:2001')
+
+print response.read()
+
+running_server.kill();
