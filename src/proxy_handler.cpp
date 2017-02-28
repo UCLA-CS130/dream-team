fc87e36c8836386d7b4e61f410896c2f130bf5c9
mjchen04 <michael.jchen04@gmail.com>
2017-02-28 11:10:54 -0800

    Get response from proxy

--- src/proxy_handler.cpp
+++ src/proxy_handler.cpp
@@ -5,6 +5,13 @@
 
 #include "proxy_handler.h"
 #include "parsed_config.h"
+#include <istream>
+#include <ostream>
+#include <string>
+#include <vector>
+#include <boost/bind.hpp>
+#include <boost/asio.hpp>
+#include "utils.h"
 
 RequestHandler::Status ProxyHandler::Init(const std::string& uri_prefix,
 					 const NginxConfig& config) {
@@ -25,8 +32,98 @@ RequestHandler::Status ProxyHandler::Init(const std::string& uri_prefix,
 
 RequestHandler::Status ProxyHandler::HandleRequest(const Request &request, 
 						  Response *response) {
+  try {
+    std::cout << "Host: " << proxy_host_ << std::endl;
+    std::cout << "Port: " << proxy_port_ << std::endl;
+    proxy_host_ = "www.boost.org";
+    proxy_port_ = "http";
+    std::string body_;
 
-  std::cout << "Host: " << proxy_host_ << std::endl;
-  std::cout << "Port: " << proxy_port_ << std::endl;
+    boost::asio::io_service io_service;
+
+    // Get a list of endpoints corresponding to the server name.
+    boost::asio::ip::tcp::resolver resolver(io_service);
+    boost::asio::ip::tcp::resolver::query query(proxy_host_, proxy_port_);
+    boost::asio::ip::tcp::resolver::iterator endpoint_iterator = resolver.resolve(query);
+
+    // Try each endpoint until we successfully establish a connection.
+    boost::asio::ip::tcp::socket socket(io_service);
+    boost::asio::connect(socket, endpoint_iterator);
+
+    // Form the request. We specify the "Connection: close" header so that the
+    // server will close the socket after transmitting the response. This will
+    // allow us to treat all data up until the EOF as the content.
+    boost::asio::streambuf request;
+    std::ostream request_stream(&request);
+    //TODO: Change to "/"
+    request_stream << "GET /LICENSE_1_0.txt HTTP/1.0\r\n";
+    request_stream << "Host: " << proxy_host_ << "\r\n";
+    request_stream << "Accept: */*\r\n";
+    request_stream << "Connection: close\r\n\r\n";
+
+    // Send the request.
+    boost::asio::write(socket, request);
+
+    // Read the response status line. The response streambuf will automatically
+    // grow to accommodate the entire line. The growth may be limited by passing
+    // a maximum size to the streambuf constructor.
+    boost::asio::streambuf response_;
+    boost::asio::read_until(socket, response_, "\r\n");
+
+    // Check that response is OK.
+    std::istream response_stream(&response_);
+    std::string http_version;
+    response_stream >> http_version;
+    unsigned int status_code;
+    response_stream >> status_code;
+    std::string status_message;
+    std::getline(response_stream, status_message);
+    if (!response_stream || http_version.substr(0, 5) != "HTTP/") {
+      std::cout << "Invalid response\n";
+      return RequestHandler::INVALID_CONFIG;
+    }
+    if (status_code != 200) {
+      std::cout << "Response returned with status code " << status_code << "\n";
+      return RequestHandler::INVALID_CONFIG;
+    } else {
+      response->SetStatus(Response::ResponseCode::OK);
+    }
+
+    // Read the response headers, which are terminated by a blank line.
+    boost::asio::read_until(socket, response_, "\r\n\r\n");
+
+    std::string header;
+    while (std::getline(response_stream, header) && header != "\r") {
+      std::vector<std::string> header_token;
+      tokenize(header, header_token, HEADER_KEY_VALUE_DELIMITER);
+      response->AddHeader(header_token[0], header_token[1]);
+      std::cout << "Header: " << header_token[0] << "Value: " << header_token[1] << std::endl;
+    }
+    std::cout << "\n";
+
+    // Write whatever content we already have to output.
+    if (response_.size() > 0) {
+      std::ostringstream ss;
+      ss << &response_;
+      body_ += ss.str();
+    }
+
+    // Read until EOF, writing data to output as we go.
+    boost::system::error_code error;
+    while (boost::asio::read(socket, response_, boost::asio::transfer_at_least(1), error)) {
+      std::ostringstream ss;
+      ss << &response_;
+      body_ += ss.str();
+    }
+
+    if (error != boost::asio::error::eof)
+      throw boost::system::system_error(error);
+
+    response->SetBody(body_);
+    std::cout << "BODY: " << response->GetBody() << std::endl;
+  }
+  catch (std::exception& e) {
+    std::cout << "Exception: " << e.what() << "\n";
+  }  
   return RequestHandler::OK;
 }
